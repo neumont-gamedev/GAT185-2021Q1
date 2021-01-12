532d735d9648b667e2ecdc956e3ea363a22c6420
neumont-gamedev <rmaple@neumont.edu>
2021-01-12 16:59:42 -0700

    Initial Project
    Initial Project Assets Commit

---
+++ Assets/Scripts/Factory.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+		{
+            GameObject gameObject = Instantiate(prefab, transform);
+            Destroy(gameObject, 1);
+		}
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 2.0f;
+ 
+    void Update()
+    {
+        Vector3 velocity = Vector3.zero;
+
+        velocity.x = Input.GetAxis("Horizontal");
+        velocity.z = Input.GetAxis("Vertical");
+
+        if (Input.GetButtonDown("Jump")) velocity.y = 40;
+
+        transform.position += velocity * speed * Time.deltaTime;
+    }
+}
