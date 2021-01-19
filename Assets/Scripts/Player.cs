b4f135399e6de78de79167ba846152068952f0ec
neumont-gamedev <rmaple@neumont.edu>
2021-01-18 20:37:33 -0700

    shooting gallery world

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 2.0f;
+    public Weapon[] weapons;
  
     void Update()
     {
-        Vector3 velocity = Vector3.zero;
+        //Vector3 velocity = Vector3.zero;
 
-        velocity.x = Input.GetAxis("Horizontal");
-        velocity.z = Input.GetAxis("Vertical");
+        //velocity.x = Input.GetAxis("Horizontal");
+        //velocity.z = Input.GetAxis("Vertical");
 
-        if (Input.GetButtonDown("Jump")) velocity.y = 40;
+        //if (Input.GetButtonDown("Jump")) velocity.y = 40;
 
-        transform.position += velocity * speed * Time.deltaTime;
+        //transform.position += velocity * speed * Time.deltaTime;
+
+        if (Input.GetButton("Fire1"))
+		{
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            weapons[0].Fire(ray.origin, ray.direction);
+        }
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public float fireRate = 0.1f;
+  
+    
+    int ammo = 100;
+    float fireTimer = 0;
+
     public GameObject bullet;
 
     void Start()
@@ -13,12 +19,27 @@ public class Weapon : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        fireTimer += Time.deltaTime;
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            GameObject gameObject = Instantiate(this.bullet, transform.position, Quaternion.identity);
-            gameObject.GetComponent<Bullet>().Fire(ray.direction);
-        }
+        //    GameObject gameObject = Instantiate(this.bullet, transform.position, Quaternion.identity);
+        //    gameObject.GetComponent<Bullet>().Fire(ray.direction);
+        //}
+    }
+
+    public bool Fire(Vector3 position, Vector3 direction)
+	{
+        if (fireTimer >= fireRate)
+		{
+            fireTimer = 0;
+            GameObject gameObject = Instantiate(this.bullet, position, Quaternion.identity);
+            gameObject.GetComponent<Bullet>().Fire(direction);
+
+            return true;
+		}
+
+        return false;
     }
 }
